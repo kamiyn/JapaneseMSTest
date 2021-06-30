f3713c21e13a37101e866d19df9ccf62c936dc09
TANABE Hiroyasu <h@cosat.com>
2021-06-30 09:58:23 +0900

    reproduction of the failure on VS2022

---
+++ UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JapaneseMSTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(false, "IsTrue TestMethod1()");
+        }
+
+        [TestMethod]
+        public void Japaneseテストメソッド1()
+        {
+            Assert.IsTrue(false, "IsTrue Japaneseテストメソッド1()");
+        }
+    }
+}
